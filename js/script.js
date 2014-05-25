1459b23563a4d4cde77ef8926265fa7cd192e7eb
shihsan <shihsan8648@yahoo.com.tw>
2014-05-26 01:07:12 +0800

    post

--- js/script.js
+++ js/script.js
@@ -173,9 +173,9 @@ window.fbAsyncInit = function () {
 };
 
 /*--------Post-----START---------------------------------------------------*/
-function PostImageToFacebook(accessToken) {
+function PostImageToFacebook(e) {
 	var postMSG="Your message";
-	var url='https://graph.facebook.com/albumID/photos?access_token='+accessToken+"&message="+postMSG;
+	var url='https://graph.facebook.com/albumID/photos?access_token='+e+"&message="+postMSG;
 	var imgURL="http://farm4.staticflickr.com/3332/3451193407_b7f047f4b4_o.jpg";
 	var formData = new FormData();
 	formData.append("url",imgURL);
