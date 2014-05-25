1673b3a58486425c8221e1d613f69a0ade9caac6
shihsan <shihsan8648@yahoo.com.tw>
2014-05-25 21:26:49 +0800

    draw img

--- js/script.js
+++ js/script.js
@@ -56,8 +56,8 @@ window.fbAsyncInit = function () {
 			});
 */
 			FB.api('/me/picture?type=large', function(response) {  // normal/large/squere
-				var str="<img id="profile_pic" src="+ response.data.url +">";
-				$('#profile').append(str);
+				//var str="<img src="+ response.data.url +">";
+				$('#profile').attr("src",response.data.url);
 			});
 
 					
