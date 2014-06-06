4380009fe9fe6360f2c36e1c8a45b71ebcd44907
shihsan <shihsan8648@yahoo.com.tw>
2014-06-07 00:20:31 +0800

    word

--- js/script.js
+++ js/script.js
@@ -151,12 +151,12 @@ window.fbAsyncInit = function () {
 			
             ctx.drawImage(profileIMG , canMouseX , canMouseY );
             ctx.drawImage(img2, 0 , 0);
-            ctx.drawImage(img3, 225 , 400);
+            ctx.drawImage(img3, 125 , 350 ); //225 , 400);
 
             var inputedText = $('#inputed').val();
             ctx.fillStyle = "black";
             ctx.font = '20px "微軟正黑體"';
-            ctx.fillText(inputedText, 300 , 445);
+            ctx.fillText(inputedText, 200 , 395);
         }
     }
 
